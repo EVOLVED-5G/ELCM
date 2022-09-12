9bb6257b4b3f123839921e464f3483b9aed3d01d
nanitebased <bgg@uma.es>
2022-09-12 11:38:59 +0200

    Allow not setting a pattern on UpgradeVerdict

--- Executor/Tasks/Run/upgrade_verdict.py
+++ Executor/Tasks/Run/upgrade_verdict.py
@@ -8,7 +8,7 @@ class UpgradeVerdict(Task):
         super().__init__("Upgrade Verdict", parent, params, logMethod, None)
         self.paramRules = {
             'Key': (None, True),
-            'Pattern': (None, True),
+            'Pattern': (None, False),
             'VerdictOnMissing': ("NotSet", False),
             'VerdictOnMatch': ("NotSet", False),
             'VerdictOnNoMatch': ("NotSet", False),
@@ -21,7 +21,8 @@ class UpgradeVerdict(Task):
         if None in [onMiss, onMatch, onNoMatch]: return
 
         key = self.params["Key"]
-        regex = re.compile(self.params["Pattern"])
+        pattern = self.params["Pattern"]
+        regex = re.compile(pattern) if pattern is not None else None
         collection = self.parent.Params
 
         if key not in collection.keys():
@@ -30,7 +31,7 @@ class UpgradeVerdict(Task):
             self.Verdict = onMiss
         else:
             value = str(collection[key])
-            if regex.match(value):
+            if regex is None or regex.match(value):
                 condition = "matches"
                 verdict = onMatch
             else:
